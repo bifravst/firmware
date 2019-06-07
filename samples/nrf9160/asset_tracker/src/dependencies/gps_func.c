d294dc3bd163095137cedeb50e3f513d7f38fcad
Simen <simensr@stud.ntnu.no>
2019-06-07 11:06:39 +0200

    added dependencies for gps functionality

---
+++ samples/nrf9160/asset_tracker/src/dependencies/gps_func.c
@@ -0,0 +1,10 @@
+#include <zephyr.h>
+#include <stdio.h>
+
+#include <gps_func.h>
+
+//GPS help functions
+
+void dummy_function_gps(void) {
+    printk("denne printen er kalt i fra gps_func modulen\n");
+}
---
+++ samples/nrf9160/asset_tracker/src/dependencies/gps_func.h
@@ -0,0 +1,6 @@
+#include <zephyr.h>
+#include <stdio.h>
+
+//gps header file for help functions
+
+void dummy_function_gps(void);
