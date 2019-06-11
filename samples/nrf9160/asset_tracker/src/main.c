d15d291d8672e31593201f15b26a70476a146e57
Simen <simensr@stud.ntnu.no>
2019-06-11 15:16:50 +0200

    started adding modem configuration

--- samples/nrf9160/asset_tracker/src/main.c
+++ samples/nrf9160/asset_tracker/src/main.c
@@ -12,8 +12,43 @@
 #include <event_manager.h>
 #include <logging/log.h>
 #include <measurement_event.h> //configuration file for event manager
+#include <misc/reboot.h>
+#include <net/mqtt.h>
+#include <net/socket.h>
+#include <lte_lc.h>
+
+void bsd_recoverable_error_handler(uint32_t err)
+{
+	printk("bsdlib recoverable error: %u\n", err);
+}
+
+/**@brief Irrecoverable BSD library error. */
+void bsd_irrecoverable_error_handler(uint32_t err)
+{
+	printk("bsdlib irrecoverable error: %u\n", err);
+
+	__ASSERT_NO_MSG(false);
+}
 
 LOG_MODULE_REGISTER(MODULE);
+
+static void modem_configure(void)
+{
+#if defined(CONFIG_LTE_LINK_CONTROL)
+	if (IS_ENABLED(CONFIG_LTE_AUTO_INIT_AND_CONNECT)) {
+		/* Do nothing, modem is already turned on
+		 * and connected.
+		 */
+	} else {
+		int err;
+
+		printk("LTE Link Connecting ...\n");
+		err = lte_lc_init_and_connect();
+		__ASSERT(err == 0, "LTE link could not be established.");
+		printk("LTE Link Connected!\n");
+	}
+#endif
+}
 
 static void generate_event(void) {
 	struct measurement_event *event = new_measurement_event();
@@ -58,15 +93,16 @@ static void buttons_leds_init(void)
 	#endif
 }
 
-int main(void)
+void main(void)
 {
 
 	printk("The application has started\n");
 	buttons_leds_init();
 	event_manager_init();
+	modem_configure();
 
-	while(1) {
-		//this section needs to include some sort of power saving, we will get to that
-	}	
-	return 0;
+	// while(1) {
+	// 	//this section needs to include some sort of power saving, we will get to that
+	// }	
+	// return 0;
 }
