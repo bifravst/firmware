b82ed0bca9be89f5d948ccb5a35b352a5bd323d3
Simen <simensr@stud.ntnu.no>
2019-06-12 11:34:06 +0200

    samples: nrf9160: asset_tracker: Added mqtt functionality

--- samples/nrf9160/asset_tracker/src/main.c
+++ samples/nrf9160/asset_tracker/src/main.c
@@ -17,6 +17,23 @@
 #include <net/socket.h>
 #include <lte_lc.h>
 
+/* Buffers for MQTT client. */
+static u8_t rx_buffer[CONFIG_MQTT_MESSAGE_BUFFER_SIZE];
+static u8_t tx_buffer[CONFIG_MQTT_MESSAGE_BUFFER_SIZE];
+//static u8_t payload_buf[CONFIG_MQTT_PAYLOAD_BUFFER_SIZE];
+
+/* The mqtt client struct */
+static struct mqtt_client client;
+
+/* MQTT Broker details. */
+static struct sockaddr_storage broker;
+
+/* Connected flag */
+static bool connected;
+
+LOG_MODULE_REGISTER(MODULE);
+
+#if defined(CONFIG_BSD_LIBRARY)
 void bsd_recoverable_error_handler(uint32_t err)
 {
 	printk("bsdlib recoverable error: %u\n", err);
@@ -29,8 +46,201 @@ void bsd_irrecoverable_error_handler(uint32_t err)
 
 	__ASSERT_NO_MSG(false);
 }
+#endif
 
-LOG_MODULE_REGISTER(MODULE);
+static void data_print(u8_t *prefix, u8_t *data, size_t len)
+{
+	char buf[len + 1];
+
+	memcpy(buf, data, len);
+	buf[len] = 0;
+	printk("%s%s\n", prefix, buf);
+}
+
+/**@brief Function to publish data on the configured topic
+ */
+static int data_publish(struct mqtt_client *c, enum mqtt_qos qos,
+	u8_t *data, size_t len)
+{
+	struct mqtt_publish_param param;
+
+	param.message.topic.qos = qos;
+	param.message.topic.topic.utf8 = CONFIG_MQTT_PUB_TOPIC;
+	param.message.topic.topic.size = strlen(CONFIG_MQTT_PUB_TOPIC);
+	param.message.payload.data = data;
+	param.message.payload.len = len;
+	param.message_id = sys_rand32_get();
+	param.dup_flag = 0;
+	param.retain_flag = 0;
+
+	data_print("Publishing: ", data, len);
+	printk("to topic: %s len: %u\n",
+		CONFIG_MQTT_PUB_TOPIC,
+		(unsigned int)strlen(CONFIG_MQTT_PUB_TOPIC));
+
+	return mqtt_publish(c, &param);
+}
+
+void mqtt_evt_handler(struct mqtt_client *const c,
+		      const struct mqtt_evt *evt)
+{
+	//int err;
+
+	switch (evt->type) {
+	case MQTT_EVT_CONNACK:
+		if (evt->result != 0) {
+			printk("MQTT connect failed %d\n", evt->result);
+			break;
+		}
+
+		connected = true;
+		printk("[%s:%d] MQTT client connected!\n", __func__, __LINE__);
+		//subscribe(); //nessecary to subscribe?
+		break;
+
+	case MQTT_EVT_DISCONNECT:
+		printk("[%s:%d] MQTT client disconnected %d\n", __func__,
+		       __LINE__, evt->result);
+
+		connected = false;
+		break;
+
+	case MQTT_EVT_PUBLISH: 
+	//{
+	// 	const struct mqtt_publish_param *p = &evt->param.publish;
+
+	// 	printk("[%s:%d] MQTT PUBLISH result=%d len=%d\n", __func__,
+	// 	       __LINE__, evt->result, p->message.payload.len);
+	// 	err = publish_get_payload(c, p->message.payload.len);
+	// 	if (err >= 0) {
+	// 		data_print("Received: ", payload_buf,
+	// 			p->message.payload.len);
+	// 		/* Echo back received data */
+	// 		data_publish(&client, MQTT_QOS_1_AT_LEAST_ONCE,
+	// 			payload_buf, p->message.payload.len);
+	// 	} else {
+	// 		printk("mqtt_read_publish_payload: Failed! %d\n", err);
+	// 		printk("Disconnecting MQTT client...\n");
+
+	// 		err = mqtt_disconnect(c);
+	// 		if (err) {
+	// 			printk("Could not disconnect: %d\n", err);
+	// 		}
+	// 	}
+	// }
+
+	break;
+
+	case MQTT_EVT_PUBACK:
+		if (evt->result != 0) {
+			printk("MQTT PUBACK error %d\n", evt->result);
+			break;
+		}
+
+		printk("[%s:%d] PUBACK packet id: %u\n", __func__, __LINE__,
+				evt->param.puback.message_id);
+		break;
+
+	case MQTT_EVT_SUBACK:
+		if (evt->result != 0) {
+			printk("MQTT SUBACK error %d\n", evt->result);
+			break;
+		}
+
+		printk("[%s:%d] SUBACK packet id: %u\n", __func__, __LINE__,
+				evt->param.suback.message_id);
+		break;
+
+	default:
+		printk("[%s:%d] default: %d\n", __func__, __LINE__,
+				evt->type);
+		break;
+	}
+}
+
+/**@brief Resolves the configured hostname and
+ * initializes the MQTT broker structure
+ */
+static void broker_init(void)
+{
+	int err;
+	struct addrinfo *result;
+	struct addrinfo *addr;
+	struct addrinfo hints = {
+		.ai_family = AF_INET,
+		.ai_socktype = SOCK_STREAM
+	};
+
+	err = getaddrinfo(CONFIG_MQTT_BROKER_HOSTNAME, NULL, &hints, &result);
+	if (err) {
+		printk("ERROR: getaddrinfo failed %d\n", err);
+
+		return;
+	}
+
+	addr = result;
+	err = -ENOENT;
+
+	/* Look for address of the broker. */
+	while (addr != NULL) {
+		/* IPv4 Address. */
+		if (addr->ai_addrlen == sizeof(struct sockaddr_in)) {
+			struct sockaddr_in *broker4 =
+				((struct sockaddr_in *)&broker);
+			char ipv4_addr[NET_IPV4_ADDR_LEN];
+
+			broker4->sin_addr.s_addr =
+				((struct sockaddr_in *)addr->ai_addr)
+				->sin_addr.s_addr;
+			broker4->sin_family = AF_INET;
+			broker4->sin_port = htons(CONFIG_MQTT_BROKER_PORT);
+
+			inet_ntop(AF_INET, &broker4->sin_addr.s_addr,
+				  ipv4_addr, sizeof(ipv4_addr));
+			printk("IPv4 Address found %s\n", ipv4_addr);
+
+			break;
+		} else {
+			printk("ai_addrlen = %u should be %u or %u\n",
+				(unsigned int)addr->ai_addrlen,
+				(unsigned int)sizeof(struct sockaddr_in),
+				(unsigned int)sizeof(struct sockaddr_in6));
+		}
+
+		addr = addr->ai_next;
+		break;
+	}
+
+	/* Free the address. */
+	freeaddrinfo(result);
+}
+
+/**@brief Initialize the MQTT client structure
+ */
+static void client_init(struct mqtt_client *client)
+{
+	mqtt_client_init(client);
+
+	broker_init();
+
+	/* MQTT client configuration */
+	client->broker = &broker;
+	client->evt_cb = mqtt_evt_handler;
+	client->client_id.utf8 = (u8_t *)CONFIG_MQTT_CLIENT_ID;
+	client->client_id.size = strlen(CONFIG_MQTT_CLIENT_ID);
+	client->password = NULL;
+	client->user_name = NULL;
+	client->protocol_version = MQTT_VERSION_3_1_1;
+
+	/* MQTT buffers configuration */
+	client->rx_buf = rx_buffer;
+	client->rx_buf_size = sizeof(rx_buffer);
+	client->tx_buf = tx_buffer;
+	client->tx_buf_size = sizeof(tx_buffer);
+
+	/* MQTT transport configuration */
+	client->transport.type = MQTT_TRANSPORT_NON_SECURE;
+}
 
 static void modem_configure(void)
 {
@@ -96,13 +306,30 @@ static void buttons_leds_init(void)
 void main(void)
 {
 
+	int err;
+
 	printk("The application has started\n");
 	buttons_leds_init();
 	event_manager_init();
 	modem_configure();
 
-	// while(1) {
-	// 	//this section needs to include some sort of power saving, we will get to that
-	// }	
-	// return 0;
+	client_init(&client);
+
+	err = mqtt_connect(&client);
+	if (err != 0) {
+		printk("ERROR: mqtt_connect %d\n", err);
+		return;
+	}	
+
+	// u8_t *test_data = 8;
+	// u8_t *test_length = sizeof(test_data);
+
+	// data_publish(&client, MQTT_QOS_1_AT_LEAST_ONCE, test_data, test_length);
+
+	while(1) {
+		data_publish(&client, MQTT_QOS_1_AT_LEAST_ONCE, test_data, test_length);
+		//this section needs to include some sort of power saving, we will get to that
+		k_busy_wait(10000);
+	}	
+	//return 0;
 }
