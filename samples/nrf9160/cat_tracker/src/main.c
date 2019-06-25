d205d22bc64524d94716c73675f85fae12d00da3
Simen S. Røstad <simen.rostad@nordicsemi.no>
2019-06-25 15:52:34 +0200

    samples: nrf9160: cat_tracker: started  added acceptance tests, bugfixes

--- samples/nrf9160/cat_tracker/src/main.c
+++ samples/nrf9160/cat_tracker/src/main.c
@@ -17,13 +17,13 @@
 #include <sensor.h>
 #include <gps_controller.h>
 
-#define PUBLISH_INTERVAL	30000
+#define PUBLISH_INTERVAL	0
 #define PAYLOAD_LENGTH		80
 #define NUMBER_OF_PACKAGES	1
 #define GPS_DELAYED_TIME	0
-#define TRACKER_ID			",CT3001,"
+#define TRACKER_ID			"CT3001"
 
-static char gps_dummy_string[PAYLOAD_LENGTH] = "\0";
+static char gps_dummy_string[PAYLOAD_LENGTH];
 
 static struct gps_data gps_data;
 
@@ -38,22 +38,16 @@ struct k_poll_event events[1] = {
 static struct k_work request_battery_status_work;
 static struct k_work publish_gps_data_work;
 static struct k_work delete_publish_string_and_set_led_work;
-static struct k_work gps_control_start_work;
-static struct k_work gps_control_stop_work;
 
-static void gps_control_start_work_fn(struct k_work *work)
-{
-	gps_control_start(GPS_DELAYED_TIME);
-}
-
-static void gps_control_stop_work_fn(struct k_work *work)
-{
-	gps_control_stop(GPS_DELAYED_TIME);
+static void base_string_set(char *gps_dummy_string) {
+	strcat(gps_dummy_string, TRACKER_ID);	
+	strcat(gps_dummy_string, ",");
 }
 
 static void request_battery_status_work_fn(struct k_work *work)
 {
 	request_battery_status(gps_dummy_string);
+	strcat(gps_dummy_string, ",");
 }
 
 static void publish_gps_data_work_fn(struct k_work *work)
@@ -64,17 +58,16 @@ static void publish_gps_data_work_fn(struct k_work *work)
 static void delete_publish_string_and_set_led_work_fn(struct k_work *work)
 {
 	memset(gps_dummy_string,0,strlen(gps_dummy_string));
+	base_string_set(gps_dummy_string);
 }
 
 static void work_init() {
 	k_work_init(&request_battery_status_work, request_battery_status_work_fn);
 	k_work_init(&publish_gps_data_work, publish_gps_data_work_fn);
 	k_work_init(&delete_publish_string_and_set_led_work, delete_publish_string_and_set_led_work_fn);
-	k_work_init(&gps_control_start_work, gps_control_start_work_fn);
-	k_work_init(&gps_control_stop_work, gps_control_stop_work_fn);
 }
 
-static void trigger_handler(struct device *dev, struct sensor_trigger *trig)
+static void adxl362_trigger_handler(struct device *dev, struct sensor_trigger *trig)
 {
 	switch (trig->type) {
 	case SENSOR_TRIG_THRESHOLD:
@@ -97,7 +90,7 @@ static void adxl362_init(void)
 		struct sensor_trigger trig = { .chan = SENSOR_CHAN_ACCEL_XYZ };
 
 		trig.type = SENSOR_TRIG_THRESHOLD;
-		if (sensor_trigger_set(dev, &trig, trigger_handler)) {
+		if (sensor_trigger_set(dev, &trig, adxl362_trigger_handler)) {
 			printk("Trigger set error\n");
 			return;
 		}
@@ -120,14 +113,14 @@ static void lte_connect(void)
 	}
 
 	lte_lc_psm_req(true);
-
+	
 }
 
 static void publish_data(void) {
 
 	while(1) {
 		k_work_submit(&request_battery_status_work);
-		k_work_submit(&gps_control_start_work);
+		gps_control_start(GPS_DELAYED_TIME);
 
 		k_poll(events, 1, K_FOREVER);
 		if (events[0].state == K_POLL_STATE_SEM_AVAILABLE) {
@@ -144,14 +137,13 @@ static void gps_control_handler(struct device *dev, struct gps_trigger *trigger)
 
 	ARG_UNUSED(trigger);
 
-	gps_control_on_trigger(); //cancels delayed work
+	gps_control_stop(GPS_DELAYED_TIME);
+
 	gps_sample_fetch(dev);
 	gps_channel_get(dev, GPS_CHAN_NMEA, &gps_data);
 
-	strcat(gps_dummy_string, TRACKER_ID);
 	strcat(gps_dummy_string, gps_data.nmea.buf);
 
-	k_work_submit(&gps_control_stop_work);
 	k_sem_give(events[0].sem);
 
 }
@@ -163,6 +155,7 @@ void main(void)
 	work_init();
 	lte_connect();
 	adxl362_init();
+	base_string_set(gps_dummy_string);
 	gps_control_init(gps_control_handler);
 	publish_data();
 
