7c888ca4c3ddf3bf812712de138155d87a4db7b4
Simen S. Røstad <simen.rostad@nordicsemi.no>
2019-06-28 10:24:54 +0200

    samples: nrf9160: cat_tracker: Added grant timer for data publishing

--- samples/nrf9160/cat_tracker/src/main.c
+++ samples/nrf9160/cat_tracker/src/main.c
@@ -19,25 +19,25 @@
 #include <gps_controller.h>
 #include <string_manipulation.h>
 
-#define PUBLISH_INTERVAL	60000
-#define GPS_DELAYED_TIME	0
+#define PUBLISH_INTERVAL	60
 #define TRACKER_ID			"CT3001"
-#define GPS_SEARCH_TIMEOUT	360000
+#define GPS_SEARCH_TIMEOUT	360
+#define SLEEP_ACCEL_THRES	30
 
-K_MUTEX_DEFINE(my_mutex);
-
-extern bool real_time_tracking;
+static bool grant = true;
 
 static char mqtt_assembly_line_d[100] = "";
 
 static struct gps_data gps_data;
 
 K_SEM_DEFINE(my_sem, 0, 1);
+K_SEM_DEFINE(my_sem2, 0, 1);
 
-struct k_poll_event events[1] = {
-    K_POLL_EVENT_STATIC_INITIALIZER(K_POLL_TYPE_SEM_AVAILABLE,
-                                    K_POLL_MODE_NOTIFY_ONLY,
-                                    &my_sem, 0)
+struct k_poll_event events[2] = {
+	K_POLL_EVENT_STATIC_INITIALIZER(K_POLL_TYPE_SEM_AVAILABLE,
+					K_POLL_MODE_NOTIFY_ONLY, &my_sem, 0),
+	K_POLL_EVENT_STATIC_INITIALIZER(K_POLL_TYPE_SEM_AVAILABLE,
+					K_POLL_MODE_NOTIFY_ONLY, &my_sem2, 0)
 };
 
 static struct k_work request_battery_status_work;
@@ -83,11 +83,32 @@ static void lte_connect(void)
 	lte_lc_psm_req(true);
 }
 
+/* experimental */
+static void my_work_handler(struct k_work *work) {
+	grant = false;
+	printk("The cat has been idle for quite some time, go to sleep\n");
+}
+
+K_WORK_DEFINE(my_work, my_work_handler);
+
+static void my_timer_handler(struct k_timer *dummy) {
+	k_work_submit(&my_work);
+}
+
+K_TIMER_DEFINE(my_timer, my_timer_handler, NULL);
+/* experimental */
+
 static void adxl362_trigger_handler(struct device *dev, struct sensor_trigger *trig)
 {
 	switch (trig->type) {
 	case SENSOR_TRIG_THRESHOLD:
-		printk("The cat has awoken, send cat data to the broker\n");
+		printk("The cat has moved, grant publish access \n");
+
+		grant = true;
+
+		k_timer_start(&my_timer, K_SECONDS(SLEEP_ACCEL_THRES),
+			      K_SECONDS(SLEEP_ACCEL_THRES));
+
 		break;
 	default:
 		printk("Unknown trigger\n");
@@ -119,7 +140,7 @@ static void gps_control_handler(struct device *dev, struct gps_trigger *trigger)
 		case GPS_TRIG_FIX:
 			printk("gps control handler triggered!\n");
 			gps_control_on_trigger();
-			gps_control_stop(GPS_DELAYED_TIME);
+			gps_control_stop(0);
 			gps_sample_fetch(dev);
 			gps_channel_get(dev, GPS_CHAN_NMEA, &gps_data);
 			concat_structure(mqtt_assembly_line_d, gps_data.nmea.buf);
@@ -131,25 +152,6 @@ static void gps_control_handler(struct device *dev, struct gps_trigger *trigger)
 	}
 }
 
-void publish_custom_data(void) {
-	while (1) {
-		k_work_submit(&request_battery_status_work);
-		gps_control_start(GPS_DELAYED_TIME);
-		k_poll(events, 1, GPS_SEARCH_TIMEOUT);
-		if (events[0].state == K_POLL_STATE_SEM_AVAILABLE) {
-			k_sem_take(events[0].sem, 0);
-			k_work_submit(&publish_gps_data_work);
-			k_work_submit(&delete_publish_data_work);
-		} else {
-			gps_control_stop(GPS_DELAYED_TIME);
-			k_work_submit(&delete_publish_data_work);
-			printk("GPS data could not be found, deleting assembly string\n");
-		}
-		events[0].state = K_POLL_STATE_NOT_READY;
-		k_sleep(PUBLISH_INTERVAL);
-	}
-}
-
 void main(void)
 {
 
@@ -157,8 +159,28 @@ void main(void)
 	work_init();
 	lte_connect();
 	adxl362_init();
-	concat_structure(mqtt_assembly_line_d, TRACKER_ID);
+	concat_structure(mqtt_assembly_line_d, TRACKER_ID); //bad name
 	gps_control_init(gps_control_handler);
-	publish_custom_data();
 
+	k_timer_start(&my_timer, K_SECONDS(SLEEP_ACCEL_THRES),
+		      K_SECONDS(SLEEP_ACCEL_THRES));
+
+	while (1) {
+		if (grant) {
+			k_work_submit(&request_battery_status_work);
+			gps_control_start(0);
+			k_poll(events, 1, K_SECONDS(GPS_SEARCH_TIMEOUT));
+			if (events[0].state == K_POLL_STATE_SEM_AVAILABLE) {
+				k_sem_take(events[0].sem, 0);
+				k_work_submit(&publish_gps_data_work);
+				k_work_submit(&delete_publish_data_work);
+			} else {
+				gps_control_stop(0);
+				k_work_submit(&delete_publish_data_work);
+				printk("GPS data could not be found, deleting assembly string\n");
+			}
+			events[0].state = K_POLL_STATE_NOT_READY;
+			k_sleep(K_SECONDS(PUBLISH_INTERVAL));
+		}
+	}
 }
