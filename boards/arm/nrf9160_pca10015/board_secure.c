0f3f731c7dd50ae86e008ac6ea7df783b28ad4c0
Simen S. Røstad <simen.rostad@nordicsemi.no>
2019-08-22 15:52:40 +0200

    feat: added pin initial pin config for ltc3554

--- boards/arm/nrf9160_pca10015/board_secure.c
+++ boards/arm/nrf9160_pca10015/board_secure.c
@@ -6,12 +6,33 @@
 
 #include <init.h>
 #include <drivers/adp536x.h>
+#include <device.h>
+#include <gpio.h>
 
 #include <logging/log.h>
 LOG_MODULE_REGISTER(board_secure, CONFIG_BOARD_LOG_LEVEL);
 
 #define ADP536X_I2C_DEV_NAME DT_NORDIC_NRF_I2C_I2C_2_LABEL
 #define LC_MAX_READ_LENGTH 128
+
+#define HPWR 0
+#define SUSP 1
+#define FSEL 2
+#define STBY 3
+
+struct gpio_pin {
+	const char *const port;
+	const u8_t number;
+};
+
+static const struct gpio_pin ltc_pins[] = {
+	{ "HPWR", 28 },
+	{ "SUSP", 27 },
+	{ "FSEL", 29 },
+	{ "STBY", 26 },
+};
+
+static struct device *ltc_devs[ARRAY_SIZE(ltc_pins)];
 
 static int power_mgmt_init(void)
 {
@@ -81,12 +102,60 @@ static int power_mgmt_init(void)
 	return 0;
 }
 
+static int ltc3554_config(void)
+{
+	int err;
+
+	for (size_t i = 0; i < ARRAY_SIZE(ltc_pins); i++) {
+		ltc_devs[i] = device_get_binding(ltc_pins[i].port);
+
+		err = gpio_pin_configure(ltc_devs[i], ltc_pins[i].number,
+					 GPIO_DIR_OUT);
+		if (err != 0) {
+			LOG_ERR("Could not configure ltc pins");
+			return err;
+		}
+	}
+
+	err = gpio_pin_write(ltc_devs[HPWR], ltc_pins[HPWR].number, true);
+	if (err != 0) {
+		LOG_ERR("Unsuccessful setting initial state HPWR: %d\n", err);
+		return err;
+	}
+
+	err = gpio_pin_write(ltc_devs[SUSP], ltc_pins[SUSP].number, true);
+	if (err != 0) {
+		LOG_ERR("Unsuccessful setting initial state SUSP: %d\n", err);
+		return err;
+	}
+
+	err = gpio_pin_write(ltc_devs[FSEL], ltc_pins[FSEL].number, true);
+	if (err != 0) {
+		LOG_ERR("Unsuccessful setting initial state FSEL: %d\n", err);
+		return err;
+	}
+
+	err = gpio_pin_write(ltc_devs[STBY], ltc_pins[STBY].number, false);
+	if (err != 0) {
+		LOG_ERR("Unsuccessful setting initial state STBY: %d\n", err);
+		return err;
+	}
+
+	return 0;
+}
+
 static int pca10015_board_init(struct device *dev)
 {
 	int err;
 
 	err = power_mgmt_init();
 	if (err) {
+		LOG_ERR("power_mgmt_init failed with error: %d", err);
+		return err;
+	}
+
+	err = ltc3554_config();
+	if (err != 0) {
 		LOG_ERR("power_mgmt_init failed with error: %d", err);
 		return err;
 	}
