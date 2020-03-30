62662a7cbf1b9abf3a9e4cbbf06131c496c5df1c
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-03-30 18:29:02 +0200

    ci: improve version detection behaviour

--- .github/workflows/get-next-version.js
+++ .github/workflows/get-next-version.js
@@ -1,38 +1,42 @@
-const semanticRelease = require('semantic-release')
-const { promises: fs } = require('fs')
-const path = require('path')
-const { WritableStreamBuffer } = require('stream-buffers')
+const semanticRelease = require("semantic-release");
+const { promises: fs } = require("fs");
+const path = require("path");
+const { WritableStreamBuffer } = require("stream-buffers");
 
-const stdoutBuffer = new WritableStreamBuffer()
-const stderrBuffer = new WritableStreamBuffer()
+const stdoutBuffer = new WritableStreamBuffer();
+const stderrBuffer = new WritableStreamBuffer();
 
 const main = async () => {
-  const pjson = JSON.parse(await fs.readFile(path.join(process.cwd(), 'package.json')))
-  const result = await semanticRelease({
-    // Core options
-    branch: pjson.release.branch,
-    repositoryUrl: pjson.repository.url,
-    plugins: [
-      '@semantic-release/commit-analyzer'
-    ],
-    dryRun: true
-  }, {
-    cwd: process.cwd(),
-    stdout: stdoutBuffer,
-    stderr: stderrBuffer
-  })
+  const pjson = JSON.parse(
+    await fs.readFile(path.join(process.cwd(), "package.json"))
+  );
+  const result = await semanticRelease(
+    {
+      // Core options
+      branch: pjson.release.branches[0],
+      repositoryUrl: pjson.repository.url,
+      plugins: ["@semantic-release/commit-analyzer"],
+      dryRun: true,
+      ci: false
+    },
+    {
+      cwd: process.cwd(),
+      stdout: stdoutBuffer,
+      stderr: stderrBuffer
+    }
+  );
 
   if (result) {
-    const { nextRelease } = result
-    process.stdout.write(nextRelease.version)
+    const { nextRelease } = result;
+    process.stdout.write(nextRelease.version);
   } else {
-    console.error('No new release.')
-    process.stderr.write(stdoutBuffer.getContentsAsString('utf8'))
+    console.error("No new release.");
+    process.stderr.write(stdoutBuffer.getContentsAsString("utf8"));
     if (stderrBuffer.size > 0) {
-      process.stderr.write(stderrBuffer.getContentsAsString('utf8'))
+      process.stderr.write(stderrBuffer.getContentsAsString("utf8"));
     }
-    process.stdout.write(pjson.version)
+    process.stdout.write(pjson.version);
   }
-}
+};
 
-main()
+main();
